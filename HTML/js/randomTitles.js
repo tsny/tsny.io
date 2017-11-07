08537345c45e671bf09ba24606a22129d6edd0a7
tsny <tsny700@gmail.com>
2017-11-07 13:23:44 -0600

    Jquery/Random
    Added more random titles and referenced Jquery CDN in index.

--- HTML/js/randomTitles.js
+++ HTML/js/randomTitles.js
@@ -1,17 +1,23 @@
 var titles = new Array
 (
-    'Intrinscly Scattered',
-    'Test Title; Ignore',
-    'Austin = DND Lord',
-    '404, Just Kidding',
-    'More Like Destiny Poo',
-    'M-I-Z',
-    'Z-O-U',
-    'Just like Minecraft',
-    'Turn on/off again',
-    'Gramma?',
-    'Hey spacer',
-    'Remember Dead Space?'
+    'Intrinscly Scattered', 'RIP Muse',
+    'Test Title; Ignore', 'RIP Bungie.net',
+    'Austin = DND Lord', 'You may have heard of me',
+    '404, Just Kidding', 'More Like Destiny Poo',
+    'M-I-Z', 'Z-O-U',
+    'Just like Minecraft', 'Turn on/off again',
+    'Gramma?', 'Hey spacer',
+    'Remember Dead Space?', 'Will Mock was here',
+    'Tommy the tertl', 'Mae Mae the Brave',
+    'There was a lady from venus', 'Burn sky til we see lines',
+    'Remember JSRF?', 'Call me maybe',
+    'Just in time for *holiday*', 'Popculture Reference',
+    'F5 F5 F5 F5', 'Bring back space sims', 
+    'Time for school', 'How many clowns?',
+    'Cmon Josh', 'Go tigers',
+    'The tsny', 'Thats so parrfect',
+    'Home of Ryans Laugh', 'RIP Noto',
+    'Return the slab', '90s kids only'
 );
 
 function getTitle()
@@ -23,23 +29,5 @@ function getTitle()
 function setTitle()
 {
     document.title = getTitle();
-    RandomWord();
 }
 
-function RandomWord() 
-{
-    var requestStr = "http://randomword.setgetgo.com/get.php";
-
-    $.ajax({
-        type: "GET",
-        url: requestStr,
-        dataType: "jsonp",
-        jsonpCallback: 'RandomWordComplete'
-    });
-}
-
-function RandomWordComplete(data) 
-{
-    alert(data.Word);
-}
-    
